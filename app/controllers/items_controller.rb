f590ae6b4609a167e24fd6d409809f6a17aedf2e
Alex Klick <aklick12@gmail.com>
2021-08-10 22:15:28 -0600

    styling update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,11 @@
 class ItemsController < ApplicationController
   def index
-    @merchant = Merchant.find(params[:merchant_id])
+    if Merchant.exists?(params[:merchant_id])
+      @merchant = Merchant.find(params[:merchant_id])
+    else
+      flash[:alert] = ["Merchant #{params[:merchant_id]} Does Not Exist! Please choose a merchant =< #{Merchant.count}", "alert-danger"]
+      redirect_to request.env["HTTP_REFERER"]
+    end
   end
 
   def show
